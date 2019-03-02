d1139a535a2d05d980ebb25b1ed9ca91bf5dfc33
jurentie <jurentie@gmail.com>
2019-03-02 16:06:48 -0700

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id:1, name: 'Natsu'},
+    {id:2, name: 'Erza'},
+    {id:3, name: 'Lucy'},
+    {id:4, name: 'Gray'},
+    {id:5, name: 'Happy'},
+    {id:6, name: 'Juvia'}
+]
